eb87667e51fa8f883d878a0edcb8ec8fd3057e94
zhenchao125 <30752740@qq.com>
2019-10-18 23:41:15 +0800

    更新

---
+++ shell-script/hive/dwd_start_log.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+db=gmall
+hive=/opt/module/hive-1.2.1/bin/hive
+hadoop=/opt/module/hadoop-2.7.2/bin/hadoop
+
+if [[ -n $1 ]]; then
+    do_date=$1
+else
+    do_date=`date -d '-1 day' +%F`
+fi
+
