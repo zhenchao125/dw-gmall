a6611e1983bcdc516889016153757c0332f4d364
zhenchao125 <30752740@qq.com>
2019-10-19 10:20:41 +0800

    更新

--- shell-script/hive/ods_log.sh
+++ shell-script/hive/ods_log.sh
@@ -10,33 +10,11 @@ else
 fi
 
 sql="
-insert overwrite table dwd_start_log
-PARTITION (dt='$do_date')
-select
-    get_json_object(line,'$.mid') mid_id,
-    get_json_object(line,'$.uid') user_id,
-    get_json_object(line,'$.vc') version_code,
-    get_json_object(line,'$.vn') version_name,
-    get_json_object(line,'$.l') lang,
-    get_json_object(line,'$.sr') source,
-    get_json_object(line,'$.os') os,
-    get_json_object(line,'$.ar') area,
-    get_json_object(line,'$.md') model,
-    get_json_object(line,'$.ba') brand,
-    get_json_object(line,'$.sv') sdk_version,
-    get_json_object(line,'$.g') gmail,
-    get_json_object(line,'$.hw') height_width,
-    get_json_object(line,'$.t') app_time,
-    get_json_object(line,'$.nw') network,
-    get_json_object(line,'$.ln') lng,
-    get_json_object(line,'$.la') lat,
-    get_json_object(line,'$.entry') entry,
-    get_json_object(line,'$.open_ad_type') open_ad_type,
-    get_json_object(line,'$.action') action,
-    get_json_object(line,'$.loading_time') loading_time,
-    get_json_object(line,'$.detail') detail,
-    get_json_object(line,'$.extend1') extend1
-from ods_start_log
-where dt='$do_date';
+load data inpath '/origin_data/gmall/log/topic_start/$do_date' into table ${db}.ods_start_log partition(dt='$do_date');
+load data inpath '/origin_data/gmall/log/topic_event/$do_date' into table ${db}.ods_event_log partition(dt='$do_date');
 "
-${hive} -e ${sql}
+echo ${sql}
+${hive} -e "${sql}"
+
+${hadoop} jar /opt/module/hadoop-2.7.2/share/hadoop/common/hadoop-lzo-0.4.20.jar com.hadoop.compression.lzo.DistributedLzoIndexer /warehouse/gmall/ods/ods_start_log/dt=${do_date}
+${hadoop} jar /opt/module/hadoop-2.7.2/share/hadoop/common/hadoop-lzo-0.4.20.jar com.hadoop.compression.lzo.DistributedLzoIndexer /warehouse/gmall/ods/ods_event_log/dt=${do_date}
