f3dd2fc1023a336587482db38a463ead24411aaa
zhenchao125 <30752740@qq.com>
2019-10-25 08:37:28 +0800

    更新

--- shell-script/cluster/date.sh
+++ shell-script/cluster/date.sh
@@ -3,7 +3,10 @@
 # 集体设置系统时间
 for host in hadoop102 hadoop103 hadoop104 ; do
     echo ========= $host =========
-    ssh $host "echo ========= $host 修改前的时间: `date` ========= ; date -s $1 ; echo ========= $host 修改后的时间: `date` ========="
+    ssh root@$host "echo ========= $host 修改前的时间: `date` ========= ;
+                    date -s $1 ;
+                    echo ========= $host 修改后的时间: `date` =========\
+                    "
 done
 
 
